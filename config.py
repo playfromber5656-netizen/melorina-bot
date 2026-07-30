267dd9ae914fcf019abed576304f895711d931ed
playfromber5656-netizen <playfromber5656@gmail.com>
2026-07-30 20:57:41 +0330

    Create config.py

---
+++ config.py
@@ -0,0 +1,21 @@
+BOT_NAME = "ملورینا"
+
+OWNER_USERNAME = "YOUR_USERNAME"
+
+CHANNELS = [
+    ("①", "https://t.me/team_Yuri"),
+    ("②", "https://t.me/pinkii008"),
+    ("③", "https://t.me/Yuriteam77"),
+    ("④", "https://t.me/animeYuri7"),
+    ("⑤", "https://t.me/Yuri90ok"),
+]
+
+PERSONALITIES = {
+    "cute": "🌸 کیوت",
+    "kind": "💗 مهربون",
+    "naughty": "😈 شیطون",
+    "tsundere": "🙄 لجباز",
+    "sleepy": "😴 خوابالو",
+    "crybaby": "🥺 بغلی",
+    "princess": "👑 پرنسس"
+}
